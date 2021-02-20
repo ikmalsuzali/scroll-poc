cc24e5747188433f8cae7996d8504e42847c74b6
Your Name <you@example.com>
2021-02-21 05:09:17 +0800

    updating the base components

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,28 +10,61 @@
       />
     </div>
     <div class="row q-mt-xl justify-center">
-      <q-btn color="primary" size="lg" label="start scrolling" @click="startScrolling" />
+      <q-btn
+        class="q-mx-lg"
+        color="primary"
+        size="lg"
+        label="Slow"
+        @click="decreaseScrollSpeed"
+      />
+      <q-btn
+        v-if="!isScrollStarted"
+        color="primary"
+        size="lg"
+        label="start"
+        @click="startScrolling"
+      />
+      <q-btn
+        v-else="isScrollStarted"
+        color="primary"
+        size="lg"
+        label="Stop"
+        @click="stopScrolling"
+      />
+      <q-btn
+        class="q-mx-lg"
+        color="primary"
+        size="lg"
+        label="Fast"
+        @click="increaseScrollSpeed"
+      />
     </div>
-    <div class="row q-mt-xl justify-center">
-      show scrolling text here
+    <div class="row">
+      <q-scroll-area ref="scrollArea" style="width: 100%; height: 620px">
+        <div style="font-size: 50px">
+          {{ text }}
+        </div>
+      </q-scroll-area>
     </div>
   </q-page>
 </template>
 
 <script>
-import text from '../assets/text.js'
+import text from "../assets/text.js";
 
 export default {
-  name: 'PageIndex',
-  data () {
+  name: "PageIndex",
+  data() {
     return {
-      text
-    }
+      text: text,
+      isScrollStarted: false
+    };
   },
   methods: {
-    startScrolling () {
-
-    }
+    startScrolling() {},
+    stopScrolling() {},
+    decreaseScrollSpeed() {},
+    increaseScrollSpeed() {}
   }
-}
+};
 </script>
